3447652c560ecc22c36c97f86fe489450e7732fb
Tsuyoshi.A <perilla.k@gmail.com>
2023-11-27 20:51:49 +0900

    Added HOME_DIR

--- pspdev/docker-pspdev.sh
+++ pspdev/docker-pspdev.sh
@@ -2,6 +2,8 @@
 
 SUDO_USER=$(/usr/bin/logname)
 EXEC_CMD=${@:-/bin/bash}
+HOME_DIR="/home/ubuntu/pspwork/"
+
 test -t 1 && USE_INTERACTIVE="--interactive"
 test -t 1 && USE_TTY="--tty"
 
@@ -9,8 +11,8 @@ docker container run \
     ${USE_INTERACTIVE} ${USE_TTY} --rm \
     -u $(id -u $SUDO_USER):$(id -g $SUDO_USER) \
     -e DEBIAN_FRONTEND=noninteractive \
-    -v `pwd`/:/home/ubuntu/pspwork/ \
-    -w /home/ubuntu/pspwork/ \
-    ${TAG_NAME:="pspdev"} \
+    -v `pwd`/:${HOME_DIR} \
+    -w ${HOME_DIR} \
+    ${TAG_NAME:="devkitpro/devkitarm"} \
     ${EXEC_CMD}
 
